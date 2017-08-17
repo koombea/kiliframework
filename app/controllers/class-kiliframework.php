66b8f6aa78c0547f3d74a61f1e238f4f808965ce
Rhonalf Martinez <rhonalf.martinez@koombea.com>
2017-08-17 14:33:02 -0500

    Code generated styles for page blocks, fixes #9

--- app/controllers/class-kiliframework.php
+++ app/controllers/class-kiliframework.php
@@ -6,16 +6,19 @@
 include_once ( get_template_directory() . '/config/defaults.php' );
 
 //Autoload Helpers.
-foreach ( glob(get_template_directory() . '/app/helpers/*/*.php') as $module ) {
+foreach ( glob( get_template_directory() . '/app/helpers/*/*.php' ) as $module ) {
   if ( !$modulepath = $module ) {
-    trigger_error(sprintf(__('Error locating %s for inclusion', 'kiliframework'), $module), E_USER_ERROR);
+    trigger_error( sprintf( __( 'Error locating %s for inclusion', 'kiliframework' ), $module ), E_USER_ERROR );
   }
   require_once $modulepath;
 }
-unset($module, $filepath);
+unset( $module, $filepath );
 
 // Theme blocks config
 include_once ( 'class-kili-theme-blocks.php' );
+
+// Dynamic styles functions
+include_once ( 'class-kili-dynamic-styles.php' );
 
 #-----------------------------------------------------------------
 # Define the main class
@@ -25,9 +28,11 @@ include_once ( 'class-kili-theme-blocks.php' );
  */
 class KiliFramework {
   protected $default_kili_blocks;
+  protected $dynamic_styles;
 
   public function __construct() {
-    $default_kili_blocks = new Kili_Theme_Blocks();
+    $this->default_kili_blocks = new Kili_Theme_Blocks();
+    $this->dynamic_styles = new Kili_Dynamic_Styles();
     $this->add_actions();
   }
 
@@ -37,7 +42,7 @@ class KiliFramework {
    * @return void
    */
   protected function add_actions() {
-    add_action('wp_enqueue_script', array($this->default_kili_blocks, 'enqueueAdmin') );
+    add_action( 'wp_enqueue_script', array( $this->default_kili_blocks, 'enqueueAdmin' ) );
   }
   
   /**
@@ -48,7 +53,7 @@ class KiliFramework {
    */
   protected function kili_pages_blocks_init_admin( $block_options = array() ) {
     foreach ( $block_options as $key => $value ) {
-      $this->default_kili_blocks->add_blocks_to_wp($value);
+      $this->default_kili_blocks->add_blocks_to_wp( $value );
     }
   }
 
