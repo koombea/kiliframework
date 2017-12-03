db0bfbf53458f689931578a1e57b2751db02c686
fabolivark <fabian.altahona@koombea.com>
2017-12-03 17:37:08 -0500

    Add html5 tags for default kili layout

--- index.php
+++ index.php
@@ -4,15 +4,26 @@
  *
  * @package kiliframework
  */
-
-if ( ! have_posts() ) : ?>
-	<div class="alert alert-warning">
-		<?php __( 'Sorry, no results were found.', 'kiliframework' ); ?>
-	</div>
-<?php
-	get_search_form();
-endif;
-while ( have_posts() ) : the_post();
-	get_template_part( 'app/views/templates/content', get_post_type() !== 'post' ? get_post_type() : get_post_format() );
-endwhile;
-the_posts_navigation();
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+		<?php if ( ! have_posts() ) : ?>
+			<div class="kili-container">
+				<div class="kili-missing-block kili-soft">
+					Sorry, no results were found.
+				</div>
+			</div>
+		<?php endif;
+		while ( have_posts() ) : the_post();
+			get_template_part( 'app/views/templates/content', get_post_type() !== 'post' ? get_post_type() : get_post_format() );
+		endwhile;
+		the_posts_navigation();
+		wp_footer(); ?>
+	</body>
+</html>
