8c78263f15bba9677e381ccdc8caaac556ab9085
Fabian Altahona <fabolivar@gmail.com>
2018-10-15 00:43:29 -0500

    Updated theme hierarchy: Child theme support for the single page on specific category eg. single-{category}.twig

--- app/routes/route-single.php
+++ app/routes/route-single.php
@@ -6,5 +6,9 @@
  */
 
 $object = get_queried_object();
+$category = get_the_category($object->ID)[0]->slug;
+if ($category) {
+	$templates[] = "{$type}-{$category}.twig";
+}
 $templates[] = $this->get_protected_view( $object, $type );
 $templates[] = "{$type}.twig";
